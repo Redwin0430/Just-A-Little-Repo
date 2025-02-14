19e7381fa1e36c3f22b08e85e32a4b255d1b4fb4
Edwin R <redwin2000@gmail.com>
2025-02-13 21:10:11 -0500

    edited scripts js

--- script.js
+++ script.js
@@ -55,15 +55,15 @@
 */
 const messages = [
     "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "You Sure",
+    "What if I let you say check in first",
+    "Please...",
+    "What if I bought you IHOP",
+    "What about AYCE Sushi",
+    "Garden Shabu?",
+    "I'm running out of ideas here",
+    "Woowww I see how it is",
+    "-.-"
 ];
 
 let messageIndex = 0;
